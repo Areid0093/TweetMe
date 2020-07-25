b0b1ce423b57839f89694829a4e574f00134b5f2
Alec Reid <alecreid0093@gmail.com>
2020-07-25 15:27:11 -0500

    Tweet home feed + create tweet form

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,28 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
+from .forms import TweetForm
 
 
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={'form': form})
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content, 'likes': random.randint(0,125)} for x in qs]
+    data = {
+        'response': tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """REST API VIEW - Return JSON data"""
