0e0128b3513d90349a3c8e27690357dcf46a887e
Alec Reid <alecreid0093@gmail.com>
2020-07-25 11:50:41 -0500

    Base Home View setup

---
+++ todo.md
@@ -0,0 +1,23 @@
+1. Users
+    -> Register
+    -> Login
+    -> Logout
+    -> Profile
+        -> Image
+        -> Text
+        -> Follow Button
+    -> Feed
+        -> User's Feed Only?
+        -> User + who they follow?
+2. Tweets
+    -> Creating
+        -> Text
+        -> Image
+    -> Delete
+    -> Retweeting
+
+3. Following/Followers
+
+Long Term/Tools
+    -> Notifications
+    -> Messages
---
+++ tweets/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """REST API VIEW - Return JSON data"""
+    data = {
+        'id': tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message']: 'Not Found'
+        status = 404
+    return JsonResponse(data, status=status)
