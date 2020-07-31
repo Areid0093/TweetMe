5a83459bd316442c39b511a131b4511dcb0d4152
Alec Reid <alecreid0093@gmail.com>
2020-07-31 13:44:42 -0500

    Saving 2 Models w/ 1 Form & 1 View

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
-
 def login_view(request, *args, **kwargs):
     form = AuthenticationForm(request, data=request.POST or None)
     if form.is_valid():
